2694868647ffc0d54a72de7f56f4074140be2e6c
baegsihun <qorwlgml2003@naver.com>
2024-05-28 09:04:58 +0900

    Initial Commit

---
+++ PracticeUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeUI
+//
+//  Created by 백시훈 on 5/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
